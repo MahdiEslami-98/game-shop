65bbdcfa3c62a73ac54a232c550e8a266ce76fe0
Mahdi Eslami <eslami.mahdi.98@gmail.com>
2024-08-13 10:49:50 +0330

    add: routes and configs

---
+++ lib/reactQueryConfig.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 6,
+    },
+  },
+});
+
+const QueryProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
